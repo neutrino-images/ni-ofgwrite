314b54cfb38cfab820f334dfeeb39b3861dae692
vanhofen <vanhofen@gmx.de>
2017-10-17 23:06:37 +0200

    - create backup_flash.tar.gz on start and copy this tarball to new rootfs

--- flash_ext4.c
+++ flash_ext4.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <getopt.h>
 
+//NI
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 int flash_ext4_kernel(char* device, char* filename, off_t kernel_file_size, int quiet, int no_write)
 {
 	char buffer[512];
@@ -129,6 +134,51 @@ int flash_ext4_rootfs(char* filename, int quiet, int no_write)
 		return 0;
 	}
 	sync();
+
+	//NI
+	if (access("/backup_flash.tar.gz", F_OK) == 0)
+	{
+		my_printf("Found backup_flash.tar.gz\n");
+		mkdir("/oldroot_bind/var", 777); //needed?
+		ret = copy_file("/backup_flash.tar.gz", "/oldroot_bind/var/backup_flash.tar.gz");
+		if (ret != 0)
+			my_printf("Error copying backup_flash.tar.gz\n");
+	}
+	sync();
+
 	ret = chdir("/"); // needed to be able to umount filesystem
 	return 1;
+}
+
+#define BUF_SIZE 1024
+copy_file(char *from, char *to)
+{
+	int inputFd, outputFd, openFlags;
+	mode_t filePerms;
+	ssize_t numRead;
+	char buf[BUF_SIZE];
+
+	inputFd = open(from, O_RDONLY);
+	if (inputFd == -1)
+		return -1;
+
+	openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+	filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH; // rw-rw-rw-
+	outputFd = open(to, openFlags, filePerms);
+	if (outputFd == -1)
+		return -1;
+
+	while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)
+		if (write(outputFd, buf, numRead) != numRead)
+			my_printf("Error: Couldn't write whole buffer");
+
+	if (numRead == -1)
+		my_printf("Error: read");
+
+	if (close(inputFd) == -1)
+		my_printf("Error: close input");
+	if (close(outputFd) == -1)
+		my_printf("Error: close output");
+
+	return 0;
 }
