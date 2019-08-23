d226e5af84c1ccf3dd4c6492bbe5424ad8f7daa0
max_10 <max_10@gmx.de>
2019-08-24 00:36:35 +0200

    - vuzero4k and vuduo4k added
    Conflicts:
    	ofgwrite.c

--- ofgwrite.c
+++ ofgwrite.c
@@ -118,10 +118,10 @@ int find_image_files(char* p)
 			if ((strstr(entry->d_name, "kernel") != NULL
 			  && strstr(entry->d_name, ".bin")   != NULL)			// ET-xx00, XP1000, VU boxes, DAGS boxes
 			 || strcmp(entry->d_name, "uImage") == 0			// Spark boxes
-			 || (strcmp(entry->d_name, "kernel1_auto.bin") == 0 && !strcmp(vumodel, "solo4k") && multiboot_partition == 1)	// solo4k multiboot
-			 || (strcmp(entry->d_name, "kernel2_auto.bin") == 0 && !strcmp(vumodel, "solo4k") && multiboot_partition == 2)	// solo4k multiboot
-			 || (strcmp(entry->d_name, "kernel3_auto.bin") == 0 && !strcmp(vumodel, "solo4k") && multiboot_partition == 3)	// solo4k multiboot
-			 || (strcmp(entry->d_name, "kernel4_auto.bin") == 0 && !strcmp(vumodel, "solo4k") && multiboot_partition == 4))	// solo4k multiboot
+			 || (strcmp(entry->d_name, "kernel1_auto.bin") == 0 && (!strcmp(vumodel, "solo4k") || !strcmp(vumodel, "duo4k") || !strcmp(vumodel, "zero4k")) && multiboot_partition == 1)	// vusolo4k/vuduo4k/vuzero4k multiboot
+			 || (strcmp(entry->d_name, "kernel2_auto.bin") == 0 && (!strcmp(vumodel, "solo4k") || !strcmp(vumodel, "duo4k") || !strcmp(vumodel, "zero4k")) && multiboot_partition == 2)	// vusolo4k/vuduo4k/vuzero4k multiboot
+			 || (strcmp(entry->d_name, "kernel3_auto.bin") == 0 && (!strcmp(vumodel, "solo4k") || !strcmp(vumodel, "duo4k") || !strcmp(vumodel, "zero4k")) && multiboot_partition == 3)	// vusolo4k/vuduo4k/vuzero4k multiboot
+			 || (strcmp(entry->d_name, "kernel4_auto.bin") == 0 && (!strcmp(vumodel, "solo4k") || !strcmp(vumodel, "duo4k") || !strcmp(vumodel, "zero4k")) && multiboot_partition == 4))	// vusolo4k/vuduo4k/vuzero4k multiboot
 			{
 				strcpy(kernel_filename, path);
 				strcpy(&kernel_filename[strlen(path)], entry->d_name);
@@ -138,10 +138,10 @@ int find_image_files(char* p)
 			 || strcmp(entry->d_name, "rootfs.ubi") == 0)			// Zgemma H9
 */
 			if (strcmp(entry->d_name, "rootfs.tar.bz2") == 0
-			 || (strcmp(entry->d_name, "rootfs1.tar.bz2") == 0 && !strcmp(vumodel, "solo4k") && multiboot_partition == 1)	// solo4k multiboot
-			 || (strcmp(entry->d_name, "rootfs2.tar.bz2") == 0 && !strcmp(vumodel, "solo4k") && multiboot_partition == 2)	// solo4k multiboot
-			 || (strcmp(entry->d_name, "rootfs3.tar.bz2") == 0 && !strcmp(vumodel, "solo4k") && multiboot_partition == 3)	// solo4k multiboot
-			 || (strcmp(entry->d_name, "rootfs4.tar.bz2") == 0 && !strcmp(vumodel, "solo4k") && multiboot_partition == 4))	// solo4k multiboot
+			 || (strcmp(entry->d_name, "rootfs1.tar.bz2") == 0 && (!strcmp(vumodel, "solo4k") || !strcmp(vumodel, "duo4k") || !strcmp(vumodel, "zero4k")) && multiboot_partition == 1)	// vusolo4k/vuduo4k/vuzero4k multiboot
+			 || (strcmp(entry->d_name, "rootfs2.tar.bz2") == 0 && (!strcmp(vumodel, "solo4k") || !strcmp(vumodel, "duo4k") || !strcmp(vumodel, "zero4k")) && multiboot_partition == 2)	// vusolo4k/vuduo4k/vuzero4k multiboot
+			 || (strcmp(entry->d_name, "rootfs3.tar.bz2") == 0 && (!strcmp(vumodel, "solo4k") || !strcmp(vumodel, "duo4k") || !strcmp(vumodel, "zero4k")) && multiboot_partition == 3)	// vusolo4k/vuduo4k/vuzero4k multiboot
+			 || (strcmp(entry->d_name, "rootfs4.tar.bz2") == 0 && (!strcmp(vumodel, "solo4k") || !strcmp(vumodel, "duo4k") || !strcmp(vumodel, "zero4k")) && multiboot_partition == 4))	// vusolo4k/vuduo4k/vuzero4k multiboot
 			{
 				strcpy(rootfs_filename, path);
 				strcpy(&rootfs_filename[strlen(path)], entry->d_name);
@@ -1188,6 +1188,36 @@ void find_kernel_rootfs_device()
 		kernel_flash_mode = TARBZ2;
 	}
 
+	if (!strcmp(vumodel, "duo4k") && multiboot_partition)
+	{
+		if (multiboot_partition == 1)
+			strcpy(kernel_device, "/dev/mmcblk0p9");
+		else if (multiboot_partition == 2)
+			strcpy(kernel_device, "/dev/mmcblk0p11");
+		else if (multiboot_partition == 3)
+			strcpy(kernel_device, "/dev/mmcblk0p13");
+		else if (multiboot_partition == 4)
+			strcpy(kernel_device, "/dev/mmcblk0p15");
+		my_printf("Using %s as kernel device\n", kernel_device);
+		found_kernel_device = 1;
+		kernel_flash_mode = TARBZ2;
+	}
+
+	if (!strcmp(vumodel, "zero4k") && multiboot_partition)
+	{
+		if (multiboot_partition == 1)
+			strcpy(kernel_device, "/dev/mmcblk0p7");
+		else if (multiboot_partition == 2)
+			strcpy(kernel_device, "/dev/mmcblk0p9");
+		else if (multiboot_partition == 3)
+			strcpy(kernel_device, "/dev/mmcblk0p11");
+		else if (multiboot_partition == 4)
+			strcpy(kernel_device, "/dev/mmcblk0p13");
+		my_printf("Using %s as kernel device\n", kernel_device);
+		found_kernel_device = 1;
+		kernel_flash_mode = TARBZ2;
+	}
+
 	if (user_rootfs)
 	{
 		if (current_rootfs_sub_dir[0] != '\0' && multiboot_partition == -1) // box with rootSubDir feature
@@ -1217,6 +1247,36 @@ void find_kernel_rootfs_device()
 			strcpy(rootfs_device, "/dev/mmcblk0p9");
 		else if (multiboot_partition == 4)
 			strcpy(rootfs_device, "/dev/mmcblk0p11");
+		my_printf("Using %s as rootfs device\n", rootfs_device);
+		found_rootfs_device = 1;
+		rootfs_flash_mode = TARBZ2;
+	}
+
+	if (!strcmp(vumodel, "duo4k") && multiboot_partition)
+	{
+		if (multiboot_partition == 1)
+			strcpy(rootfs_device, "/dev/mmcblk0p10");
+		else if (multiboot_partition == 2)
+			strcpy(rootfs_device, "/dev/mmcblk0p12");
+		else if (multiboot_partition == 3)
+			strcpy(rootfs_device, "/dev/mmcblk0p14");
+		else if (multiboot_partition == 4)
+			strcpy(rootfs_device, "/dev/mmcblk0p16");
+		my_printf("Using %s as rootfs device\n", rootfs_device);
+		found_rootfs_device = 1;
+		rootfs_flash_mode = TARBZ2;
+	}
+
+	if (!strcmp(vumodel, "zero4k") && multiboot_partition)
+	{
+		if (multiboot_partition == 1)
+			strcpy(rootfs_device, "/dev/mmcblk0p8");
+		else if (multiboot_partition == 2)
+			strcpy(rootfs_device, "/dev/mmcblk0p10");
+		else if (multiboot_partition == 3)
+			strcpy(rootfs_device, "/dev/mmcblk0p12");
+		else if (multiboot_partition == 4)
+			strcpy(rootfs_device, "/dev/mmcblk0p14");
 		my_printf("Using %s as rootfs device\n", rootfs_device);
 		found_rootfs_device = 1;
 		rootfs_flash_mode = TARBZ2;
