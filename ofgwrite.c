02204ff1a3283e0dc77bc4278ca6f7691b3cbee1
vanhofen <vanhofen@gmx.de>
2023-07-27 00:14:34 +0200

    - fix merge

--- ofgwrite.c
+++ ofgwrite.c
@@ -1713,7 +1713,7 @@ int main(int argc, char *argv[])
 			my_printf("Error flashing rootfs! System won't boot. Please flash backup! System will reboot in 60 seconds\n");
 			set_error_text1("Error flashing rootfs. System won't boot!");
 			set_error_text2("Please flash backup! Rebooting in 60 sec");
-			if (stop_e2_needed)
+			if (stop_neutrino_needed)
 			{
 				sleep(60);
 				reboot(LINUX_REBOOT_CMD_RESTART);
